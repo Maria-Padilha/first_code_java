2f1311557e06bfa74d2d5baa89022cc515b1d680
maria-padilha <padilhabarbosa23@gmail.com>
2023-12-15 15:17:32 -0400

    time manipulation exercise

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -5,13 +5,20 @@ public class Exercise2 {
     public int[] numeros = {10,20,30};
 
     public static void main(String args[]){
-        
+
         Scanner scanner = new Scanner(System.in);
         double tempoAntigo = System.currentTimeMillis();
         String s = scanner.nextLine();
 
         if(Exercise2.convertTime(System.currentTimeMillis() - tempoAntigo) > 2){
-            System.out.println(s);
+            if(s.length() >= 10){
+                System.out.println(s);
+
+            }else{
+                System.out.println("vai corinthians");
+            }
+        }else{
+            System.out.println("OPA! Espera 2 segundos ein rapaz");
         }
     }
 
